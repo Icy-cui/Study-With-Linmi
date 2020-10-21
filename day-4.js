283ef0b543313aaa1e53e9a7a088a173d08870b2
cuiyunfei <icercyf@gmail.com>
2020-10-22 00:09:13 +1100

    day-4

---
+++ day-4.js
@@ -0,0 +1,19 @@
+var isPrime = function(n) {
+    let count = 0;
+    let signs = new Uint8Array(n);
+
+    for (let i = 2; i < n; i++) {
+        if (!signs[i - 1]) {
+            count++;
+
+            for (let j = i * i; j <= n; j += i) {
+                signs[j - 1] = true;
+            }
+        }
+    }
+    alert(count);
+    return count;
+};
+
+let n = prompt('give a number', '')
+isPrime(n)
