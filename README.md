cc27172322c76b2275a4f7a2cbef0b0153eb233e
cuiyunfei <icercyf@gmail.com>
2020-10-23 01:17:59 +1100

    day-5

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Study front-end course with linmi
 day-1: HTML + CSS  
 day-2: HTML + CSS  
 day-3: HTML + JavaScript  
-day-4: JavaScript: prime number count (copy)
+day-4: JavaScript: prime number count (copy)  
+day-5: JavaScript: function + switch  
---
+++ day-5.js
@@ -0,0 +1,14 @@
+let evenNum = function(n) {
+    for (let i = 1; i <= n; i++) {
+        let m = i % 2;
+        switch (m) {
+            case 1:
+                continue;
+            case 0:
+                alert(i);
+        }
+    }
+}
+
+let n = prompt('give a number', '')
+evenNum(n)
