dde690cae59b3149cc7ce071a91731aae32b1501
cuiyunfei <icercyf@gmail.com>
2020-11-08 18:15:09 +1100

    day-20

--- day-20/day-20.js
+++ day-20/day-20.js
@@ -1,5 +1,20 @@
 // go live mode
 
-document.body.style.background = 'red'; // 将背景设置为红色
+document.body.style.background = 'limegreen'; // set background colour to red
+setTimeout(() => document.body.style.background = '', 3000); // set back
 
-setTimeout(() => document.body.style.background = '', 3000); // 恢复回去
+// visit all child nodes
+for (let i = 0; i < document.body.childNodes.length; i++) {
+    console.log(document.body.childNodes[i]); // Text, DIV, Text, UL, ..., SCRIPT
+}
+
+console.log(document.body.lastChild);
+
+let l = document.getElementById('elem');
+console.log(l.children, l.firstElementChild.innerHTML = 'gg', l.lastElementChild);
+
+/**
+ * 对于所有节点：parentNode，childNodes，firstChild，lastChild，previousSibling，nextSibling。
+ * 仅对于元素节点：parentElement，children，firstElementChild，lastElementChild，previousElementSibling，nextElementSibling。
+ * "getElementsBy*"是动态的，“ querySelectorAll”是静态的
+ */
