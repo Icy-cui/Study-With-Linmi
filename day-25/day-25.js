d44dab0ac455f160273e08e2f74049aa7a9d9468
cuiyunfei <icercyf@gmail.com>
2020-11-18 01:32:05 +1100

    day-25

--- day-25/day-25.js
+++ day-25/day-25.js
@@ -5,24 +5,59 @@ function init() {
 }
 
 function Division(selector) {
+    // 单例
+    var resource = new Resource();
     this.ele = document.querySelector(selector);
     // console.log(this.ele);
-    this.add = document.querySelector('.add');
-    this.sub = document.querySelector('.sub');
+    this.add = this.ele.querySelector('.add');
+    this.sub = this.ele.querySelector('.sub');
+    // input 
+    this.input = this.ele.querySelector('input');
 
     this.add.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log(1);
+        // console.log(resource.balance, this.input.value)
+        // console.log(resource.balance);
+        resource.add(this.input.value);
+        renderBalance();
     });
 
     this.sub.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log(2);
+        resource.sub(this.input.value);
+        renderBalance();
     });
 }
 
+function renderBalance() {
+    var resource = new Resource();
+    money.innerText = resource.balance;
+}
 
+function Resource() {
+    if (Resource.instance)
+        return Resource.instance;
+    else {
+        this.balance = 100;
+        Resource.instance = this;
+    }
+}
 
+Resource.prototype.add = function(num) {
+    if (!num) {
+        return;
+    } else {
+        this.balance += parseFloat(num);
+    }
+}
+
+Resource.prototype.sub = function(num) {
+    if (!num) {
+        return;
+    } else {
+        this.balance -= parseFloat(num);
+    }
+}
 
 var money = document.getElementById('balance');
 
