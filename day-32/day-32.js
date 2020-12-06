f195bd2607c822e7ae0d691d21c81bc07ba722db
cuiyunfei <icercyf@gmail.com>
2020-12-06 23:32:46 +1100

    day-32

--- day-32/day-32.js
+++ day-32/day-32.js
@@ -1,3 +1,50 @@
+// class Music {
+//     constructor(name, hot) {
+//       this.name = name;
+//       this.hot = hot;
+//     }
+
+//     play() {
+//       console.log(this.name + ' 播放中...');
+//     }
+//   }
+
+//   class MusicList {
+//     constructor() {
+//       this.list = [];
+//       this.current = 0;
+//     }
+
+//     add(music) {
+//       this.list.push(music);
+//     }
+
+//     next(music) {
+//       return this.list[++this.current];
+//     }
+
+//     prev(music) {
+//       return this.list[--this.current];
+//     }
+
+//     onlyHot() {
+//       return this.list.filter(music => music.hot);
+//     }
+//   }
+
+//   const musicList = new MusicList();
+//   musicList.add(new Music('阿花爱上了阿蛋', true));
+//   musicList.add(new Music('我的滑板孩', true));
+//   musicList.add(new Music('辣鸡的爱', false));
+
+//   console.log(musicList.onlyHot());
+
+// console.log(musicList.next());
+// console.log(musicList.prev());
+// console.log(musicList.next());
+
+
+
 function Music(name, hot) {
     this.name = name;
     this.hot = hot;
