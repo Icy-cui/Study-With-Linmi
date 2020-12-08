5c393c78435f229114979304d666484cc28006b2
cuiyunfei <icercyf@gmail.com>
2020-12-08 21:53:04 +1100

    day-35

---
+++ JavaScript/day-35/day-35.js
@@ -0,0 +1,87 @@
+/* Form */
+class UserForm {
+    constructor(selector) {
+        this.el = document.querySelector(selector);
+        this.bindEvents();
+    }
+    bindEvents() {
+        let form = this.el;
+        let query = form.querySelector.bind(form);
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            // querySelector name= name/gender/score/quality
+            let name = query('[name=name]').value;
+            let gender = query('[name=gender]').value;
+            let score = query('[name=score]').value;
+            let quality = query('[name=quality]').value;
+            // console.log(name);
+            // validate form input
+            let builder = new Builder();
+            builder.setName(name).setGender(gender).setQuality(quality).setScore(score);
+            console.log(builder.build());
+        })
+    }
+}
+
+/* User data */
+class User {}
+
+class Builder {
+    constructor() {
+        this.instance = new User();
+    }
+
+    setName(name) {
+        if (!name) {
+            throw 'Name cannot be None!'
+        }
+        let maxLen = 15;
+        if (name.length > maxLen) {
+            throw 'Name cannot be longer than ${maxLen}!'
+        }
+        this.instance.name = name;
+        return this;
+    }
+
+    setGender(gender) {
+        let genderSet = ['M', 'F'];
+
+        if (genderSet.indexOf(gender) < 0)
+            throw 'Gender should be F or M!';
+
+        this.instance.gender = gender;
+        return this;
+    }
+
+    setScore(score) {
+        if (!this.validNum(score)) {
+            throw 'Score should be a number!'
+        }
+        this.instance.score = parseFloat(score);
+        return this;
+    }
+
+    setQuality(quality) {
+        if (!this.validNum(quality)) {
+            throw 'Quality should be a number!'
+        }
+        this.instance.quality = parseFloat(quality);
+        return this;
+    }
+    validNum(val) {
+        // if (val != 0  && !parseFloat (val)) {
+        //     return false;
+        // }
+        // return true;
+        return val === 0 || parseFloat(val);
+    }
+    build() {
+        return this.instance;
+    }
+}
+
+// let builder = new Builder();
+// builder.setName('cc').setGender('F').setQuality(100).setScore(100);
+// console.log(builder.build());
+
+let form = new UserForm('form');
