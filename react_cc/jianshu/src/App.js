d6f0848aa63d32352b45e7c2f2affa3ae7be03b2
cuiyunfei <icercyf@gmail.com>
2021-01-01 16:48:51 +1100

    janshu

---
+++ react_cc/jianshu/src/App.js
@@ -0,0 +1,15 @@
+import { Code } from './style.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <Code>src/App.js</Code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
