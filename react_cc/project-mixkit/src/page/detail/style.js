fb1001642bbdd2a7ad6e1de9a92ad54db0a80523
cuiyunfei <icercyf@gmail.com>
2021-01-25 16:08:12 +1100

    style

--- react_cc/project-mixkit/src/page/detail/style.js
+++ react_cc/project-mixkit/src/page/detail/style.js
@@ -14,8 +14,8 @@ export const MusicHeader = styled.div `
   a {
     margin-top: 1.4rem;
     padding-left: 40px;
-    font-size: 18px;
     color: #fff;
+    font-size: 16px;
     font-height: 27px;
     text-decoration-line: none;
   }
@@ -24,7 +24,7 @@ export const MusicHeader = styled.div `
 export const MusicBanner = styled.div `
   background-size: auto 90%, auto 100%;
   ${"" /* background-color: #554cb7; */}
-  background: url("https://mixkit.imgix.net/static/heroes/music/foreground.png?h=600&auto=format");
+  background-image: url("https://mixkit.imgix.net/static/heroes/music/foreground.png?h=600&auto=format");
 `;
 
 export const VideoWapper = styled.div `
